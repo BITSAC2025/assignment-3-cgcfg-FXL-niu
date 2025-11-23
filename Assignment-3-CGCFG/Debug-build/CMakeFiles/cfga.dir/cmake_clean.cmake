5e60af7530532a9a464c8686d8a57af58efd5df4
FXL-niu <2397298913@qq.com>
2025-11-23 23:50:13 +0800

    Assignment

---
+++ Assignment-3-CGCFG/Debug-build/CMakeFiles/cfga.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/fxl666/assignment-3-cgcfg-FXL-niu/Assignment-3-CGCFG/cfga"
+  "/home/fxl666/assignment-3-cgcfg-FXL-niu/Assignment-3-CGCFG/cfga.pdb"
+  "CMakeFiles/cfga.dir/CFGA.o"
+  "CMakeFiles/cfga.dir/CFGA.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cfga.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Assignment-3-CGCFG/Debug-build/CMakeFiles/cfga_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cfga_lib.dir/cfga_lib.o"
+  "CMakeFiles/cfga_lib.dir/cfga_lib.o.d"
+  "libcfga_lib.a"
+  "libcfga_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cfga_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
